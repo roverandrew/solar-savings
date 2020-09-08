caeafee98c1a9a76be1220441cf0960590c3c179
Andrew Rover <andrewrover@Andrews-MacBook-Air-2.local>
2020-09-08 01:57:40 -0400

    hiding api keys

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ app.use(express.static(buildPath));
 
 app.get('/location', async (req, res) => {
   const { latitude, longitude } = req.query;
-  let fetchedLocationData = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=de&limit=10&apiKey=db0750073ffb4979ae72e94c742cd728`);
+  let fetchedLocationData = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=de&limit=10&apiKey=${process.env.GEO_API_KEY}`);
   let locationData = await fetchedLocationData.json();
   res.json(locationData);
 });
 
 app.get('/power', async (req, res) => {
   const { latitude, longitude, systemCapacity, azimuth, tilt, arrayType, moduleType, losses } = req.query;
-  const fetchedPowerData = await fetch(`https://developer.nrel.gov/api/pvwatts/v6.json?api_key=nANSd1IKE1BAzkWI5BwrefIJDaTXhuEJd4O89gQv&lat=${latitude}&lon=${longitude}&system_capacity=${systemCapacity}&azimuth=${azimuth}&tilt=${tilt}&array_type=${arrayType}&module_type=${moduleType}&losses=${losses}`);
+  const fetchedPowerData = await fetch(`https://developer.nrel.gov/api/pvwatts/v6.json?api_key=${process.env.NREL_API_KEY}&lat=${latitude}&lon=${longitude}&system_capacity=${systemCapacity}&azimuth=${azimuth}&tilt=${tilt}&array_type=${arrayType}&module_type=${moduleType}&losses=${losses}`);
   const powerData = await fetchedPowerData.json();
   res.json(powerData);
 });
